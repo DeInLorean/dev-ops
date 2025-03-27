bfdd408bcf4b01778a2d267c134653c5fa90e8fa
DeInLorean <delorean23321@gmail.com>
2025-03-27 11:30:07 +0000

    add main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "null" {}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Terraform apply successful'"
+  }
+}
